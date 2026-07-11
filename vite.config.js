fa26caa0b1a8f23254310ff4200f130d49338bd7
majid3230 <mohdmajid75893@gmail.com>
2026-07-11 19:30:45 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import {defineConfig} from 'vite';
+
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    server: {
+      hmr: process.env.DISABLE_HMR !== 'true',
+      watch: process.env.DISABLE_HMR === 'true' ? null : {},
+      port: 3000,
+      host: '0.0.0.0'
+    },
+  };
+});
