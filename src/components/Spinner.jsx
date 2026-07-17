28ac99b3543b0cab96a4d883397fb79a5f9fdd2a
majid3230 <mohdmajid75893@gmail.com>
2026-07-17 23:11:54 +0530

    Update Spinner.jsx

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React from "react";
 import styles from "../styles/Spinner.module.css";
 
-const Spinner = ({ fullScreen = false }) => {
-  return (
-    <div className={`${styles.container} ${fullScreen ? styles.fullScreen : ''}`}>
-      <div className={styles.spinner}></div>
-    </div>
-  );
-};
+
+function Spinner(){
+
+return (
+
+<div className={styles.spinner}>
+
+<div className={styles.circle}></div>
+
+<p>
+Loading...
+</p>
+
+</div>
+
+);
+
+}
+
 
 export default Spinner;
