77c5a27285e3646ceb3bf15fb69e400ed49283a3
majid3230 <mohdmajid75893@gmail.com>
2026-07-18 22:45:18 +0530

    Update useToast.js

--- src/hooks/useToast.js
+++ src/hooks/useToast.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { ToastContext } from '../components/Toast/ToastProvider';
+import { ToastContext } from "../components/ToastProvider.jsx";
 
 export const useToast = () => {
   const context = useContext(ToastContext);
